1f84c98828f0b1993fe2c494581e4adc53c199a3
Steve Long <steven989@gmail.com>
2016-10-16 19:16:52 -0400

    Create Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+    def dashboard
+        
+    end
+
+    def new_connection
+        
+    end
+
+    def create_connection
+        
+    end
+
+    def new_activity
+        
+    end
+
+    def create_activity
+        
+    end
+end
