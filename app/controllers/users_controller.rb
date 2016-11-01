c5fee27036edb2f2f5d818698154ecd4cd2aef15
Steve Long <steven989@gmail.com>
2016-11-01 18:51:01 -0400

    Add functionalities for basic user creation and connection creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,37 @@
 class UsersController < ApplicationController
+    before_action :require_login, except: [:new, :create]
+
+
+    def new
+        @user = User.new    
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          auto_login(@user)
+          redirect_to :root 
+        else
+          render action: :new
+        end        
+    end
+
     def dashboard
-        
+        @connection = Connection.new
     end
 
     def new_connection
-        
+       @connection = Connection.new 
     end
 
     def create_connection
-        
+        connection = Connection.new(connection_params)
+        if connection.save
+            connection.update_attributes(user_id: current_user.id)
+            redirect_to root_path, notice: "Successfully created"
+        else
+            redirect_to root_path, alert: "Could not be created"
+        end
     end
 
     def new_activity
@@ -17,5 +40,18 @@ class UsersController < ApplicationController
 
     def create_activity
         
+    end
+
+    private
+    def connection_params
+        params.require(:connection).permit(:first_name,:last_name)
+    end
+
+    def user_params
+        params.permit(:email, :password, :password_confirmation)
+    end
+
+    def require_login
+        redirect_to login_path if !logged_in?
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   has_many :connections
-  has_many :activities
+  has_many :activities  
+
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  validates :email, uniqueness: true
 end
