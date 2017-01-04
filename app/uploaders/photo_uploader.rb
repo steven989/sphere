b29d291aa8f3e86703d684a7a2833f385d0a65b0
Steve Long <steven989@gmail.com>
2017-01-04 20:28:23 +0100

    Fix the no update issue with S3. Turns out file name has to change each time

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -11,7 +11,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{Rails.env}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -47,7 +47,13 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    "#{model.class.to_s.underscore}_id#{model.id}_display_photo.#{file.extension}" if original_filename
+    "#{secure_token}.jpg" if original_filename.present?
+  end
+
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
   end
 
 end
